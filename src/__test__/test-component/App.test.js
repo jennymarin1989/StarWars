2bd15f77f5b0fcf28e9bc19d20252ab67cc5cbab
jose <jennymarin1989@hotmail.com>
2018-07-01 18:46:59 +0100

    :white_check_mark:Add first test for App.js component

---
+++ src/__test__/test-component/App.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../../components/App';
+
+describe('App', () => {
+
+  describe('renders correctly', () => {
+    it('renders components correctly', () => {
+      let app = shallow(<App />);
+    expect(app).toMatchsnapshot();  
+    })
+  })
+
+
+})
