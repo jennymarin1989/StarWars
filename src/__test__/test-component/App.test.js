1a790e04cef0b7e533b8a0aea50cbcd988fa9b2d
jose <jennymarin1989@hotmail.com>
2018-07-01 22:31:33 +0100

    :white_check_mark:Add test for rendering title child component in App.js

--- src/__test__/test-component/App.test.js
+++ src/__test__/test-component/App.test.js
@@ -5,11 +5,17 @@ import App from '../../components/App';
 describe('App', () => {
 
   describe('renders correctly', () => {
+
     it('renders components correctly', () => {
       let app = shallow(<App />);
     expect(app).toMatchSnapshot();  
-    })
-  })
+    });
+  
+    it('renders Title component correctly', () => {
+      let app = shallow(<App />);
+     expect(app.find('title')).toHaveLength(1);
+    });
 
+  });
 
 })
