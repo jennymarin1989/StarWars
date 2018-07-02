6e28d385e74bfdc205e33070ec7e066f99e2cc81
jose <jennymarin1989@hotmail.com>
2018-07-02 16:37:36 +0100

    :construction: Added consditional rendering in App.js so renders the searchPage is condition is true - Failing test

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Title from '../components/Title';
 import Button from '../components/Button';
+import SearchPage from '../components/SearchPage';
 
 class App extends Component {
   constructor(){
@@ -26,6 +27,7 @@ class App extends Component {
           image={"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/1200px-Star_Wars_Logo.svg.png"}
           onClick={this.handleClick}
         />
+        {this.state.starwars ? <SearchPage /> : null}
       </div>  
     )
   }
