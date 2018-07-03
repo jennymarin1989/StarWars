6732153c30a410ad826d097651e801984ffd848f
jose <jennymarin1989@hotmail.com>
2018-07-03 16:04:28 +0100

    :lipstick::package: Add style-components package, pass test to render TyleTitle component

--- src/__test__/test-component/StyleTitle.test.js
+++ src/__test__/test-component/StyleTitle.test.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import styled from 'styled-components'
-import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme';
 import 'jest-styled-components'
+import StyleTitle from '../../components/styleComponents/StyleTitle'
 
 describe('StyleTitle', () =>{
 
-  let title = render.create(<StyleTitle />).toJSON();
-
   it('returns title with specific style', ()=>{
+    let title = shallow(<StyleTitle />)
     expect(title).toMatchSnapshot();
     expect(title).toHaveStyleRule('color', 'red', 'border', 'white');
   })
---
+++ src/components/styleComponents/StyleTitle.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const StyleTitle = styled.text `
+    font-size: 0.8em;
+    color: red;
+    font-family: verdana;
+    margin: 25px;
+    border: 25px;
+`;
+
+export default StyleTitle;
